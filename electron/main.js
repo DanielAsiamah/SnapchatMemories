6e932a84abf7a075a0d78b6e3767b2007d819f9c
Daniel Asiamah <chinaman1998@hotmail.com>
2026-06-30 01:20:54 +0100

    Security: Locked down Electron config for production

--- electron/main.js
+++ electron/main.js
@@ -8,8 +8,10 @@ function createWindow() {
     width: 1200,
     height: 800,
     webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
+      nodeIntegration: false,
+      contextIsolation: true,
+      sandbox: true,
+      enableRemoteModule: false
     },
     icon: path.join(__dirname, '../public/snapchat-logo.png')
   });
